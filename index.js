bf977b52a5e28ad06b9b5f344f6ce943ad14018f
KarenSandova <sandovalk061@gmail.com>
2024-06-18 11:02:10 -0500

    PROJECT

---
+++ index.js
@@ -0,0 +1,12 @@
+function onClickSubmitButton(){
+    let inputValue = document.getElementById('inputText').value;
+    if(inputValue>0){
+        alert ("hey el número es mayor a 0")
+    }
+    if(inputValue>0){
+        alert ("hey el número es menor a 0")
+    }
+    if(inputValue==0){
+        alert ("intente otra vez")
+    }
+}
